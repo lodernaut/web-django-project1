ca4ddabe438bddc5b50818eb565ddd8a827674bb
Gabriel Lemos <gabriellemosdev@gmail.com>
2022-11-16 11:27:37 -0300

    upgrade

--- recipes/views.py
+++ recipes/views.py
@@ -5,6 +5,6 @@ from django.shortcuts import render
 # django está retornando renderizado a request e o caminho template
 # HTTP REQUEST, TEMPLATE_NAME = caminho do template
 def home(request):
-    return render(request, "global/home.html", context= {
+    return render(request, "recipes/pages/home.html", context={
         "name": "Gabriel Lemos",
-    }) 
+    })
