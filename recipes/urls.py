0e30661d146f3c1918d334bb4b183ff62b4ba1c8
Gabriel Lemos <gabriellemosdev@gmail.com>
2022-11-21 12:25:02 -0300

    final-test

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
+# recipes-recipe
+app_name = "recipes"
+
 urlpatterns = [
-    path("", views.home),  # Home
-    path("recipes/<int:id>/", views.recipe),  # recipe
+    path("", views.home, name="recipes-home"),  # Home
+    # recipe, criando caminho dinâmico → name="recipes-recipe"
+    path("recipes/<int:id>/", views.recipe, name="recipes-recipe"),
 ]
