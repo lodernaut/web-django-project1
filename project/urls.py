1e0ba544628cd405e2cdf86e62a49ef3fb807703
Gabriel Lemos <gabriellemosdev@gmail.com>
2023-01-21 12:43:42 -0300

    http Request / return http Response

--- project/urls.py
+++ project/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+# http Request <- return http Response
+def my_view(Request):
+    return HttpResponse("TESTE RETORNO")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("sobre/", my_view),
+
 ]
