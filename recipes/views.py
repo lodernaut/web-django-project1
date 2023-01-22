69a4dbee274a8396296d2750672d38043e204179
Gabriel Lemos <gabriellemosdev@gmail.com>
2023-01-22 12:39:44 -0300

    separating pages

--- recipes/views.py
+++ recipes/views.py
@@ -5,6 +5,6 @@ from django.shortcuts import render
 
 # http Request <- return http Response
 def home(request):
-    return render(request, "recipes/home.html", context={
+    return render(request, "recipes/pages/home.html", context={
         "name": "Gabriel Lemos",
     })
