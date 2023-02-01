64a2fcfe5509901bf109d1497b78416c0d325085
Gabriel Lemos <gabriellemosdev@gmail.com>
2023-02-01 12:23:58 -0300

    create global statics

--- project/settings.py
+++ project/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
+    "django.contrib.staticfiles",  # arquivos estáticos
     "recipes",
 ]
 
@@ -119,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "base_static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
