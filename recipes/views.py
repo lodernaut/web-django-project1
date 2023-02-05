db6fb706a7ef19dd0e976d101ebcad10939af5a7
Gabriel Lemos <gabriellemosdev@gmail.com>
2023-02-05 00:42:06 -0300

    add {{ recipe.cover.url }}

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 # Create your views here.
 
 
 # http Request <- return http Response
 def home(request):
-    return render(request, "recipes/pages/home.html")
+    return render(request, "recipes/pages/home.html", context={
+        # list comprehension. ↓
+        "recipes": [make_recipe for _ in range(11)],
+    })
 
 
 def recipe(request, id):
-    return render(request, "recipes/pages/recipe-view.html")
+    return render(request, "recipes/pages/recipe-view.html", context={
+        "recipe": make_recipe(),
+    })
