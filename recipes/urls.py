5d8869618fa7c54a2cff7d556105ef163efddbf3
Gabriel Lemos <gabriellemosdev@gmail.com>
2023-02-05 23:13:13 -0300

    create - namespace / app_name = "recipes"

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from . import views
 
+# recipes:recipes
+app_name = "recipes"
+
 # http Request <- return http Response
 urlpatterns = [
-    path("", views.home),  # domínio/recipes  → # home
+    path("", views.home, name="home"),  # domínio/recipes  → # home
     # passando paramento <id> → para → views.recipe
-    path("recipes/<int:id>/", views.recipe),
+    path("recipes/<int:id>/", views.recipe, name="recipe"),
 ]
