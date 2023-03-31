fa31c6f8c264acbcc38d728aad4c5e37d64745df
Gabriel Lemos <gabriellemosdev@gmail.com>
2023-03-31 11:05:15 -0300

    updates

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
-
-from utils.recipes.factory import make_recipe
+from django.shortcuts import get_list_or_404, render
 
 from .models import Recipe
 
@@ -16,16 +14,23 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category
-        __id=category_id, is_published=True).order_by('-id')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id, is_published=True,).order_by('-id'))
+
     return render(request, "recipes/pages/category.html", context={
-        "recipes": recipes
+        "recipes": recipes,
+        # como é uma lista e não uma queryset buscar o índice [0] e não first()
+        "title": f"{recipes[0].category.name}",
     })
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True,).order_by('-id').first()
+
     return render(request, "recipes/pages/recipe-view.html", context={
-        "recipe": make_recipe(),
+        "recipe": recipe,
         "is_detail_page": True,
     })
