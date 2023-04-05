13fa2f63cdc863db12aa3cb865b5a72b1abbe4f0
Gabriel Lemos <gabriellemosdev@gmail.com>
2023-04-05 09:38:54 -0300

    settings pytest

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class RecipeURLsTest(TestCase):
+    def test_recipe_home_url_is_correct(self):
+        url = reverse("recipes:home")
+        # testando se url é igual a barra
+        self.assertEqual(url, "/")
