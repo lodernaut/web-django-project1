ba6af4a7e0d7392f9e3114469362846d6cce69cd
Gabriel Lemos <gabriellemosdev@gmail.com>
2023-04-06 15:31:50 -0300

    testing status code

--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -1,10 +1,7 @@
 from django.test import TestCase
-from django.urls import resolve, reverse
-
-from . import views
+from django.urls import reverse
 
 
-# Create your tests here.
 class RecipeURLsTest(TestCase):
     def test_recipe_home_url_is_correct(self):
         url = reverse("recipes:home")
@@ -20,18 +17,3 @@ class RecipeURLsTest(TestCase):
     def test_recipe_detail_url_is_correct(self):
         url = reverse("recipes:recipe", args=(1,))
         self.assertEqual(url, "/recipes/1/")
-
-
-class RecipeViewsTest(TestCase):
-    # teste: função de view → 'home' está correta?
-    def test_recipe_home_view_function_is_correct(self):
-        view = resolve(reverse("recipes:home"))
-        self.assertIs(view.func, views.home)
-
-    def test_recipe_category_view_function_is_correct(self):
-        view = resolve(reverse("recipes:category", kwargs={"category_id": 1}))
-        self.assertIs(view.func, views.category)
-
-    def test_recipe_detail_view_function_is_correct(self):
-        view = resolve(reverse("recipes:recipe", args=(1,)))
-        self.assertIs(view.func, views.recipe)
