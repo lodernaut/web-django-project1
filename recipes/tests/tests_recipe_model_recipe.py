423148a4bf56e1bc236cf08efaccb221aba15323
Gabriel Lemos <gabriellemosdev@gmail.com>
2023-04-10 13:36:08 -0300

    added parameterized

--- recipes/tests/tests_recipe_model_recipe.py
+++ recipes/tests/tests_recipe_model_recipe.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ValidationError
+from parameterized import parameterized
 
 from .test_recipe_base import RecipeTestBase
 
@@ -11,5 +12,16 @@ class RecipeModelTest(RecipeTestBase):
     def test_recipe_title_raises_error_if_title_has_more_than_65_chars(self):
         self.recipe.title = "X"*66
 
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
+
+    @parameterized.expand([
+        ("title", 65),
+        ("description", 165),
+        ("preparation_time_unit", 65),
+        ("servings_unit", 65),
+    ])
+    def test_recipe_max_length(self, field, max_length):
+        setattr(self.recipe, field, "X" * (max_length + 1))
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
--- z-save.py
+++ z-save.py
@@ -1,4 +1,10 @@
-x = {"marcos": 123}
-y = {"pedro": 456}
-
-print(*x)
+if __name__ == "__main__":
+    fields = [
+        ("title", 65),
+        ("description", 165),
+        ("preparation_time_unit", 65),
+        ("servings_unit", 65),
+    ]
+    for field, max_length in fields:
+        x = "x"*(max_length+1)
+        print(field, x, len(x))
