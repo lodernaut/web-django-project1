23e2b7f427b734b4ecf191e26b2c874860fabe25
Gabriel Lemos <gabriellemosdev@gmail.com>
2023-04-23 17:59:02 -0300

    test mock - context manage

--- recipes/tests/test_recipe_home_view.py
+++ recipes/tests/test_recipe_home_view.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from django.urls import resolve, reverse
 
 from recipes import views
@@ -52,3 +54,44 @@ class RecipeHomeViewTest(RecipeTestBase):
         self.assertIn(
             "<h1>No recipes found here</h1>", response.content.decode("utf-8"))
         self.assertEqual(len(response_context_recipes), 0)
+
+    # mock
+
+    @patch("recipes.views.PER_PAGE_HOME", new=3)
+    def test_recipe_home_is_paginator_mock(self):
+
+        for i in range(9):  # for para 9 receitas criadas
+            kwargs = {
+                "author_data": {"username": f"username_test{i+1}"},
+                "slug": f"slug-test-{i+1}"}
+            self.make_recipe(**kwargs)
+
+        response = self.client.get(reverse("recipes:home"))
+        recipes = response.context["recipes"]
+
+        paginator = recipes.paginator
+
+        # criou 9 receitas → retornar 3 pág
+        self.assertEqual(paginator.num_pages, 3)
+        # Verificando quantas receitas tem na pág 1, esperado 3
+        self.assertEqual(len(paginator.get_page(1)), 3)
+
+    def test_recipe_home_is_paginator_context_manage(self):
+        for i in range(9):  # for para 9 receitas criadas
+            kwargs = {
+                "author_data": {"username": f"username_test{i+1}"},
+                "slug": f"slug-test-{i+1}"}
+            self.make_recipe(**kwargs)
+
+        # context manage
+        with patch("recipes.views.PER_PAGE_HOME", new=3):
+            response = self.client.get(reverse("recipes:home"))
+            recipes = response.context["recipes"]
+
+            paginator = recipes.paginator
+
+            # criou 9 receitas → retornar 3 pág
+            self.assertEqual(paginator.num_pages, 3)
+
+            # Verificando quantas receitas tem na pág 1, esperado 3
+            self.assertEqual(len(paginator.get_page(1)), 3)
---
+++ z-save.py
@@ -0,0 +1,2 @@
+for i in range(6):
+    print(f"slug-test-{i+1}")
