ccf57626a25f34989245d08fc0bee6575d127033
Gabriel Lemos <gabriellemosdev@gmail.com>
2023-06-06 13:44:27 -0300

    continuation form

--- authors/views.py
+++ authors/views.py
@@ -1,17 +1,28 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import redirect, render
 
 from .forms import RegisterForm
 
 
 # Create your views here.
 def register_view(request):
-    if request.POST:
-        # passando dados para dentro do formulário
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+    register_form_data = request.session.get(
+        "register_form_Data", None)  # padrão já é None
+    form = RegisterForm()
 
     return render(
         request, "authors/pages/register_view.html", {
             "form": form,
         })
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404
+
+    POST = request.POST
+    # Salvando dicionário do POST inteiro
+    request.session["register_form_data"] = POST
+    form = RegisterForm(POST)
+
+    return redirect("authors:register")
