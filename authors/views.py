eb7078a3b0c28f31da1351bdc210a33898c22b04
Gabriel Lemos <gabriellemosdev@gmail.com>
2023-06-12 13:10:55 -0300

    data - form.save()

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import redirect, render
 
@@ -24,5 +25,12 @@ def register_create(request):
     # Salvando dicionário do POST inteiro
     request.session["register_form_data"] = POST
     form = RegisterForm(POST)
+
+    if form.is_valid():  # se o formulário é valido
+        form.save()  # salvando dados do formulário na base de dados
+        messages.success(request, "Your user is created, please log in.")
+
+        # deletando/limpando chave do dicionário
+        del (request.session["register_form_data"])
 
     return redirect("authors:register")
