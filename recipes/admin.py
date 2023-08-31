d7b1ca6c56f93a78bb99beb0fcf5d2b248a7d39f
Gabriel Lemos <gabriellemosdev@gmail.com>
2023-08-31 20:15:14 -0300

    adding roles in django admin area

--- recipes/admin.py
+++ recipes/admin.py
@@ -11,7 +11,17 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)  # decorator
 class RecipeAdmin(admin.ModelAdmin):
-    pass
+    list_display = "id", "title", "author", "created_at", "is_published",
+    list_display_links = [
+        "id", "title", "author", "created_at"]  # adicionando link's
+    # adicionando campos de busca/ definindo quais campos a ser buscado
+    search_fields = "id", "title", "description", "slug", "preparation_step",
+    # adicionando filter
+    list_filter = [
+        "is_published", "category", "author", "preparation_step_is_html"]
+    # list_per_page = 5  # Caso queira controlar quantidade de  visível por pág
+    list_editable = "is_published",
+    # ordering = "id",  # se quiser ordenar
 
 
 admin.site.register(Category, CategoryAdmin)  # 1º model, 2º category
