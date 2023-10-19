fcad86db240c528ee2557227d82de13253f92c97
Gabriel Lemos <gabriellemosdev@gmail.com>
2023-10-18 21:16:15 -0300

    update errors

--- project/settings.py
+++ project/settings.py
@@ -80,12 +80,9 @@ WSGI_APPLICATION = "project.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    << << << < HEAD
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        == == == =
-
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
